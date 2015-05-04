643c219b0e71196c3a31d79d6dc5952ee81f12ff
liamhand <l.hand112@hotmail.com>
2015-05-04 05:21:59 +0100

    Commented app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,13 @@
+//angular.module can be thought of as a container for my the different parts of my app
+//this is used as a replacement for a main method as these different parts can be reused and loaded at any time in any order in my app, and do not follow a strict pattern
 angular.module('calorieCounter', ['ionic', 'project.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
+
+ //Defines what states my app can be in. Each state has a controller which is found in controllers.js
   $stateProvider
   
+  //My default homepage state
   .state('home', {
     url: "/home",
     abstract: false,
@@ -11,6 +16,7 @@ angular.module('calorieCounter', ['ionic', 'project.controllers'])
   })
   
 
+  //the categories in my side-bar
   .state('app', {
     url: "/app",
     abstract: true,
@@ -18,7 +24,7 @@ angular.module('calorieCounter', ['ionic', 'project.controllers'])
     controller: 'AppCtrl'
   })
   
-
+  //all dishes and the calorie counter itself
   .state('app.dishes', {
     url: "/dishes/:dishesId",
     views: {
@@ -29,7 +35,7 @@ angular.module('calorieCounter', ['ionic', 'project.controllers'])
     }
   });
   
-  
+  //This is the default state '/home'. This is used when the app is first opened or when another state is not recognised
   $urlRouterProvider.otherwise('/home');
 
 });
